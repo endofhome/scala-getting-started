703e2e91a423cd63c90a0940b6d2d1b9342cf69b
Tom Barnes <ruralmyth@gmail.com>
2016-02-29 18:02:36 +0000

    refractoring: infix notation. only allowed as no side-effects.

--- src/test/scala/FilterCheckerTests.scala
+++ src/test/scala/FilterCheckerTests.scala
@@ -7,14 +7,14 @@ class FilterCheckerTests extends FlatSpec{
   "return a list with that file" in {
     val matchingFile = new FileObject("match")
     val listOfFiles = List(new FileObject("random"), matchingFile)
-    val matchedFiles = FilterChecker("match").findMatchedFiles(listOfFiles)
+    val matchedFiles = FilterChecker("match") findMatchedFiles listOfFiles
     assert(matchedFiles == List(matchingFile))
   }
   
   "FilterChecker passed a list with a directory that matches the filter" should 
   "should not return the directory" in {
     val listofIOObjects = List(new FileObject("random"), new DirectoryObject("match"))
-    val matchedFiles = FilterChecker("match").findMatchedFiles(listofIOObjects)
+    val matchedFiles = FilterChecker("match") findMatchedFiles listofIOObjects
     assert(matchedFiles.length == 0)
   }
 }
