d2e8bc3b937b50b6dd5a9f559b08abed6679fe52
Tom Barnes <ruralmyth@gmail.com>
2016-03-02 16:05:29 +0000

    FilterChecker passed files with matching/non-matching content: tests passing

--- src/main/scala/FilterChecker.scala
+++ src/main/scala/FilterChecker.scala
@@ -1,6 +1,7 @@
 package fileSearcher
 
 import java.io.File
+import scala.util.control.NonFatal
 
 class FilterChecker(filter: String) {
   def matches(content : String) = content contains filter
@@ -10,6 +11,23 @@ class FilterChecker(filter: String) {
         if(iOObject.isInstanceOf[FileObject])
         if(matches(iOObject.name)))
     yield iOObject
+    
+  def matchesFileContent(file: File) = {
+    import scala.io.Source
+    try {
+      val fileSource = Source.fromFile(file)
+      try
+        fileSource.getLines() exists(line=>matches(line))
+      catch {
+        case NonFatal(_) => false
+      }
+      finally
+        fileSource.close()
+    }
+    catch {
+      case NonFatal(_) => false
+    }
+  } 
 }
 
 object FilterChecker {
