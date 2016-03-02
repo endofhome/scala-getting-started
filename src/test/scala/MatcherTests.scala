91096610b8e8c47299bb7e60ed81374f8013543f
Tom Barnes <ruralmyth@gmail.com>
2016-03-02 22:36:16 +0000

    Matcher returns a file name with match count using Tuples

--- src/test/scala/MatcherTests.scala
+++ src/test/scala/MatcherTests.scala
@@ -8,14 +8,14 @@ class MatcherTests extends FlatSpec {
   "return a list with that file name" in {
     val matcher = new Matcher("fake", "fakePath")
     val results = matcher.execute()
-    assert(results == List("fakePath"))
+    assert(results == List(("fakePath", None)))
   }
   
   "Matcher using a directory containing one file matching the filter" should
   "return a list with that file name" in {
     val matcher = new Matcher("txt", new File("." + File.separator + "testfiles").getCanonicalPath())
     val results = matcher.execute()
-    assert(results == List("testFile.txt"))
+    assert(results == List(("testFile.txt", None)))
   }
   
   "Matcher that is not passed a root file location" should
@@ -30,7 +30,7 @@ class MatcherTests extends FlatSpec {
     val matcher = new Matcher("txt", new File("." + File.separator + "testfiles").getCanonicalPath(), 
                              searchSubDirectories)
     val results = matcher.execute()
-    assert(results == List("subDirTestFile.txt", "testFile.txt"))
+    assert(results == List(("subDirTestFile.txt", None), ("testFile.txt", None)))
   }
   
   "Matcher given a path that has one file that matches file filter and content filter" should
@@ -38,13 +38,13 @@ class MatcherTests extends FlatSpec {
     val matcher = new Matcher("data", new File(".").getCanonicalPath(), true,
                               Some("pluralsight"))
     val matchedFiles = matcher.execute()
-    assert(matchedFiles == List("pluralsight.data"))
+    assert(matchedFiles == List(("pluralsight.data", Some(3))))
   }
   
   "Matcher given a path that has no file that matches file filter and content filter" should
   "return an empty list" in {
     val matcher = new Matcher("txt", new File(".").getCanonicalPath(), true,
-                              Some("pluralsight"))
+                              Some("pluralsight")) 
     val matchedFiles = matcher.execute()
     assert(matchedFiles == List())
   }
